fbb31de68ca2f3117aa551e7d408f8c14537d72c
Pavel Barashkov <Pav.Barashkov@yandex.ru>
2023-05-07 22:02:00 +0400

    remove url cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 
 
-app.use(cors({ origin: 'https://itransition-final-project-client-7z3d0qocs-pavelbarashkov.vercel.app', credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use('/api', router);
 
